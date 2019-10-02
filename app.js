4c9c2025c782c18cef43e11bab468ce0de2f0ec0
Tom Cummings <tdgcummings@gmail.com>
2019-10-02 21:41:03 +0100

    Big push

--- app.js
+++ app.js
@@ -11,25 +11,29 @@ Vue.component('sw-club', {
       <div class="column col-3">\
         <label class="form-label">{{ clubSw }}</label>\
       </div>\
-      <div class="column col-3">\
+      <div class="column col-2">\
         <input v-model.number="clubWeight" class="form-input" type="text" placeholder="384">\
       </div>\
-      <div class="column col-3">\
-        <input v-model.number="clubBalPnt" class="form-input" type="text" placeholder="29.65">\
+      <div class="column col-2">\
+        <input v-model.number="clubBalPnt" class="form-input" type="text" placeholder="29">\
+      </div>\
+      <div class="column col-2">\
+        <input v-model.number="clubBalPntFrac" class="form-input" type="text" placeholder="4">\
       </div>\
     </div>',
+  props: [
+    'index',
+  ],
+  created: function() {
+  },
   data: function() {
     return {
-      message: 'Hello Vue!',
-      clubName: '',
-      clubWeight: '',
-      clubBalPnt: ''
     };
   },
   computed: {
     clubSw: function() {
       var finalSw;
-      finalSw = this.clubWeight * (this.clubBalPnt - 14);
+      finalSw = this.clubWeight * ( (this.clubBalPnt + (this.clubBalPntFrac / 16) ) - 14);
       finalSw = (finalSw * 0.03528 - 143.5) / 1.75;
       
       finalSw = Math.round(finalSw * 10) / 10;
@@ -45,7 +49,39 @@ Vue.component('sw-club', {
       }
       
       return 'Error with values entered';
-    }
+    },
+    clubName: {
+      get: function() {
+        return this.$parent.swRows[this.index].clubName;
+      },
+      set: function(test) {
+        this.$parent.swRows[this.index].clubName = test;
+      },
+    },
+    clubWeight: {
+      get: function() {
+        return this.$parent.swRows[this.index].clubWeight;
+      },
+      set: function(test) {
+        this.$parent.swRows[this.index].clubWeight = test;
+      },
+    },
+    clubBalPnt: {
+      get: function() {
+        return this.$parent.swRows[this.index].clubBalPnt;
+      },
+      set: function(test) {
+        this.$parent.swRows[this.index].clubBalPnt = test;
+      },
+    },
+    clubBalPntFrac: {
+      get: function() {
+        return this.$parent.swRows[this.index].clubBalPntFrac;
+      },
+      set: function(test) {
+        this.$parent.swRows[this.index].clubBalPntFrac = test;
+      },
+    },
   },
   methods: {
   }
@@ -54,22 +90,78 @@ Vue.component('sw-club', {
 var vm = new Vue({ /* jshint ignore:line */
   el: '#app',
   data: {
-    swRows: 5,
+    swRows: [],
     isActive: false
+  },
+  mounted: function() {
+    for (var i = 0; i < 5; i++) {
+      this.createRow();
+    }
   },
   computed: {
   },
   methods: {
+    createRow: function() {
+      var templateData = {
+        clubName: '',
+        clubWeight: '',
+        clubBalPnt: '',
+        clubBalPntFrac: ''
+      };
+      this.swRows.push(templateData);
+    },
     addRow: function() {
-      this.swRows += 1;
+      this.createRow();
       this.$refs.addRow.blur();
     },
     deleteRow: function() {
-      if (this.swRows > 1) {
-        this.swRows -= 1;
+      if (this.swRows.length > 1) {
+        this.swRows.pop();
       }
       this.$refs.deleteRow.blur();
-    }
+    },
+    saveData: function() {
+      var a = document.createElement('a');
+      a.style = 'display: none';
+      var data = JSON.stringify({
+        clubs: this.swRows,
+      }, null, 2);
+      var blob = new Blob([data], {type: 'application/json'});
+      var url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = 'club-sw.json';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+      this.$refs.saveData.blur();
+    },
+    loadData: function(ev) {
+      // if (window.confirm("Are you sure, all unsaved data will be lost")) { return; }
+      
+      var reader = new FileReader();
+    
+      var readSuccess = function(ev) {
+        var data = null; 
+        try {
+          data = JSON.parse(ev.target.result);
+        } catch (e) {
+          window.alert('Cannot load file, please check and try again');
+          return;
+        }
+
+        this.swRows = [];
+        var clubs = data.clubs;
+        for (var i = 0; i < clubs.length; i++) {
+          this.swRows.push(clubs[i]);
+        }
+        
+        this.$refs.loadData.value = '';
+      }.bind(this);
+      reader.onload = readSuccess;
+    
+      reader.readAsText(ev.target.files[0]);
+    },
   }
 });
   
